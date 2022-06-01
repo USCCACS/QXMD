6e1799bb5396101cc20de9a3a7584729af56fa18
Anikeya <37962685+Anikeya@users.noreply.github.com>
2022-06-01 14:23:36 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,5 +4,5 @@ mkdir build
 ./configure --prefix=/scratch1/tlinker/WORKSHOP/QXMD/fftw-3.3.8/build CC=gcc CFLAGS=-O3 MPICC=mpicc F77=mpif77 F90=mpif90 FFLAGS=-O3 --enable-shared=yes --enable-sse2 --enable-avx --enable-avx2 --disable-avx512 --disable-avx-128-fma --disable-kcvi --disable-altivec --disable-vsx --disable-neon --disable-generic-simd128 --disable-generic-simd256 --disable-fma --enable-static=yes --enable-mpi --enable-openmp --enable-threads
 make -j 2
 make install
-
-
+cd ../QXMD/
+make qxmd

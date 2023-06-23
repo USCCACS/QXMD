aa0d36f9bbfe4ebc6ddd570e8f0828ba08cd87c5
Anikeya <anikeya@Anikeya-fedora.usc.edu>
2023-06-23 14:50:54 -0700

    updated the run.sh files, june 23, 2023

--- Examples/03_NAQMD/01_Water_NVE/run.sh
+++ Examples/03_NAQMD/01_Water_NVE/run.sh
@@ -1,7 +1,7 @@
 cp ../../../qxmd .
-cp /content/QXMD-CYBER-MAGICS-2022JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2022JUNE/Examples/03_NAQMD/01_Water_NVE/analysis/bond-angle/.
-cp /content/QXMD-CYBER-MAGICS-2022JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2022JUNE/Examples/03_NAQMD/01_Water_NVE/analysis/eig/.
-cp /content/QXMD-CYBER-MAGICS-2022JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2022JUNE/Examples/03_NAQMD/01_Water_NVE/analysis/rate/.
+cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/03_NAQMD/01_Water_NVE/analysis/bond-angle/.
+cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/03_NAQMD/01_Water_NVE/analysis/eig/.
+cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/03_NAQMD/01_Water_NVE/analysis/rate/.
 mkdir data
 export PYTHONPATH=../../../util/
 export LD_LIBRARY_PATH=../../../fftw-3.3.8/build/lib:${LD_LIBRARY_PATH}

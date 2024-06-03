757d15c725ffeea8279a5d77856c4ae33b5aa89e
Logan Yamamoto Mac <lkyamamo@usc.edu>
2024-06-03 09:47:13 -1000

    changed path references in example run scripts

--- Examples/01_Optimization/01_Water/run.sh
+++ Examples/01_Optimization/01_Water/run.sh
@@ -1,7 +1,7 @@
 cp ../../../qxmd .
-cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/01_Optimization/01_Water/analysis/DOS/.
-cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/01_Optimization/01_Water/analysis/eng/.
-cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/01_Optimization/01_Water/analysis/pdb/.
+cp /content/QXMD/util/qxREAD.py /content/QXMD/Examples/01_Optimization/01_Water/analysis/DOS/.
+cp /content/QXMD/util/qxREAD.py /content/QXMD/Examples/01_Optimization/01_Water/analysis/eng/.
+cp /content/QXMD/util/qxREAD.py /content/QXMD/Examples/01_Optimization/01_Water/analysis/pdb/.
 export PYTHONPATH=../../../util/
 export LD_LIBRARY_PATH=../../../fftw-3.3.8/build/lib:${LD_LIBRARY_PATH}
 mkdir data
--- Examples/02_Adaibatic_MD/01_Water_NVE/run.sh
+++ Examples/02_Adaibatic_MD/01_Water_NVE/run.sh
@@ -1,6 +1,6 @@
 cp ../../../qxmd .
-cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/02_Adaibatic_MD/01_Water_NVE/analysis/bond-angle/.
-cp /content/QXMD-CYBER-MAGICS-2023JUNE/util/qxREAD.py /content/QXMD-CYBER-MAGICS-2023JUNE/Examples/02_Adaibatic_MD/01_Water_NVE/analysis/eng/.
+cp /content/QXMD/util/qxREAD.py /content/QXMD/Examples/02_Adaibatic_MD/01_Water_NVE/analysis/bond-angle/.
+cp /content/QXMD/util/qxREAD.py /content/QXMD/Examples/02_Adaibatic_MD/01_Water_NVE/analysis/eng/.
 mkdir data
 export PYTHONPATH=../../../util/
 export LD_LIBRARY_PATH=../../../fftw-3.3.8/build/lib:${LD_LIBRARY_PATH}
